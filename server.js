d931da7a41f160729396172c5b813958a18e3916
beekay2706 <93442895+beekay2706@users.noreply.github.com>
2022-03-06 17:58:28 +0530

    returning error message

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ app.get("/", function (req, res) {
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+  console.log("Hi Barath");
+});
+
+app.get("api/timestamp/:datestring", function(req, res){
+       res.json({"error":"invalid Date"});
 });
 
 
