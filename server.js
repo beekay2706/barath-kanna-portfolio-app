17a71f3262b8d05107a75c4367cf410296ffc874
beekay2706 <93442895+beekay2706@users.noreply.github.com>
2022-03-06 21:01:27 +0530

    Updating html

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/timestamp", function (req, res) {
+  res.sendFile(__dirname + '/views/timestamp.html');
+});
+
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
