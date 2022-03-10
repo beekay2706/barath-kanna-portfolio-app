7cf668610575ea9a8b76d854484a09009432c911
beekay2706 <93442895+beekay2706@users.noreply.github.com>
2022-03-11 04:50:25 +0530

    Add mongoose and mongodb

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
+var database_uri = "mongodb+srv://Beekay2706:Maggibeekay2706@cluster0.zlnnw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 // server.js
 // where your node app starts
 
 // init project
 var express = require('express');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var shortId = require('shortid');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 var app = express();
 var port = process.env.PORT || 3000;
+
+// mongoose.connect(process.env.DB_URI;)
+mongoose.connect(database_uri,{useNewUrlParser: true, 
+  useUnifiedTopology: true
+});
+var shortUrl = mongoose.model("shortUrl", 
+  new mongoose.Schema({
+    short_url: String,
+    original_url: String,
+    suffix: String
+}));
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -12,6 +29,12 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
@@ -24,13 +47,48 @@ app.get("/timestamp", function (req, res) {
 app.get("/requestheaderparser", function (req, res) {
   res.sendFile(__dirname + '/views/requestheaderparser.html');
 });
-
+app.get("/urlshortner", function (req, res) {
+  res.sendFile(__dirname + '/views/urlshortner.html');
+});
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
   console.log("Hi Barath");
 });
+// Url shortener microservice 
 
+app.post("/api/shorturl/", (req, res) => {
+  let client_requested_url = req.body.url
+
+  let suffix = shortId.generate();
+  let newShortURL = suffix
+
+  let newURL = new shortUrl({
+    short_url: __dirname + "/api/shorturl/" + suffix,
+    original_url: client_requested_url,
+    suffix: suffix
+  })
+
+  newURL.save((err, doc) => {
+    if (err) return console.error(err);
+    res.json({
+      "saved": true,
+      "short_url": newURL.short_url,
+      "orignal_url": newURL.original_url,
+      "suffix": newURL.suffix
+    });
+  });
+});
+
+app.get("/api/shorturl/:suffix", (req, res) => {
+  let userGeneratedSuffix = req.params.suffix;
+  shortUrl.find({suffix: userGeneratedSuffix}).then(foundUrls => {
+    let urlForRedirect = foundUrls[0];
+    res.redirect(urlForRedirect.original_url);
+  });
+});
+
+// time stamp microservice - 1
 app.get("/api/", function(req,res){
   let then = new Date();
  res.json({
@@ -39,6 +97,8 @@ app.get("/api/", function(req,res){
   });
 
 });
+
+// Header parser microservice
 app.get("/api/whoami", function(req, res){
   res.json(
     {
@@ -48,6 +108,7 @@ app.get("/api/whoami", function(req, res){
     });
 
 });
+// time stamp microservice - 2
 app.get("/api/:date_string", function(req, res){
        let dateString = req.params.date_string;
 
